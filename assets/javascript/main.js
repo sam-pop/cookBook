8fe1331225dfdc63ba4cef85a551975b61857dda
sam-pop <poplovitch.samuel@gmail.com>
2018-05-21 16:26:27 -0400

    test

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,22 +4,26 @@ var appKey = "b47a9edb6afcce664d0df80592628d5a"; // edamam application key
 var searchParam = "chicken%20flour%20bread"; // the search param to use for the api query //TODO: change this field
 var space = "%20"; // use this instead of spaces between worsd
 
+$(function () {
+
+});
 
 $(document).ready(function () {
-
-    // API
+    // API url
     var apiURL = "https://api.edamam.com/search?app_id=" + appID + "&app_key=" + appKey + "&q=" + searchParam;
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        async: "false",
-        url: apiURL, //API url
-        success: function (json) { //TODO: build the success function
-            console.log(json.hits); //FIXME: TEST ONLY
+    // API
+    // $.ajax({
+    //     type: "GET",
+    //     dataType: "json",
+    //     async: "false",
+    //     url: apiURL, //API url
+    //     success: function (json) { //TODO: build the success function
+    //         console.log(json.hits); //FIXME: TEST ONLY
 
-        },
-        error: function () { //TODO: build meaningful error function
-            alert("API error!");
-        }
-    });
+    //     },
+    //     error: function () { //TODO: build meaningful error function
+    //         alert("API error!");
+    //     }
+    // });
+    //  END OF api
 });
