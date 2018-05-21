470af1b9247030c7b32daf755db0ed10d90d448a
sam-pop <poplovitch.samuel@gmail.com>
2018-05-21 14:22:05 -0400

    api update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,16 +1,21 @@
 // Gloval Variables
-var appID = "36b1ec01";
-var appKey = "b47a9edb6afcce664d0df80592628d5a";
-var searchParam = "chicken%20flour%20bread";
-var apiURL = "https://api.edamam.com/search?app_id=" + appID + "&app_key=" + appKey + "&q=" + searchParam;
+var appID = "36b1ec01"; // edamam application ID
+var appKey = "b47a9edb6afcce664d0df80592628d5a"; // edamam application key
+var searchParam = "chicken%20flour%20bread"; // the search param to use for the api query
+var space = "%20"; // use this instead of spaces between worsd
 
-// API
+
+
+
 $(document).ready(function () {
+
+    // API
+    var apiURL = "https://api.edamam.com/search?app_id=" + appID + "&app_key=" + appKey + "&q=" + searchParam;
     $.ajax({
         type: "GET",
         dataType: "json",
         async: "false",
-        url: apiURL,
+        url: apiURL, //API url
         success: function (json) {
             console.log(json.hits[1]);
 
